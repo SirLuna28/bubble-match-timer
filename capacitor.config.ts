68038da0483c37ee095187614e49a4765af2dd03
SirLuna28 <kobeluna71@gmail.com>
2026-06-17 12:18:00 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.loujjstudio.bubblematchtime',
+ appId: 'Space Bubble Match - LouJJStudios',
   appName: 'Bubble Match Timer',
   webDir: 'dist/public'
 };
